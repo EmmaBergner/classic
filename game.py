e7acbe48d98637ab1f5097de178a9c0cf5d275cc
EmmaBergner <bergner.emma@gmail.com>
2022-03-13 19:41:46 +0000

    First version

---
+++ game.py
@@ -0,0 +1,75 @@
+word = "Soup"
+
+
+def make_padding(failed, correct):
+    """
+    Create a string that is used as padding. The length of this string 
+    and the length of the two parameter strings will be the sum 60.
+    Example: make_padding("[d]", "_ a _ _ _") returns 54 spaces. 
+    """
+    padding = " "
+    length = 60 - len(failed) - len(correct)
+    for i in range(length):
+        padding = padding + " "
+    return padding
+
+
+def character_position(string, letter_from_user):
+    """
+    Returns right position for character
+    """
+    result = []
+    for i in range(len(string)):
+        if string[i] == letter_from_user:
+            result.append(i)
+    return result
+
+
+def wrong_letter(string, letter_from_user):
+    """
+   Retuns wrong letter at right position 
+    """
+    result = []
+    for i in range(len(string)):
+        if string[i] != letter_from_user:
+            result.append(string[i])
+            print(string[i])
+    return result
+
+
+def user_input():
+    """Create a input field for user to enter a letter.
+    Repeats for 8 times.
+    """
+    failed = ""
+    letter_in_word = "_ _ _ _ _ _ _ _ _ _ "
+    for i in range(3):
+        letter_from_user = input("Type a letter: ")
+        failed = failed + "[ " + letter_from_user + " ]"
+        correct = correct_hits(letter_in_word, letter_from_user)
+        wrong = wrong_letter(word, letter_from_user)
+        padding = make_padding(wrong, correct) 
+        print(failed + padding + correct + "\n")
+        letter_in_word = correct
+
+
+def correct_hits(correct, letter_from_user):
+    """ Place the letter in correct place """
+    hits = (character_position(word, letter_from_user))
+    for hit in range(len(hits)):
+        index = hits[hit] * 2
+        correct = correct[:index] + letter_from_user + correct[index + 1:]
+    return correct
+
+
+def main():
+    """ Run all program function """
+    user_input()
+
+
+""" Program starts here """
+print("Welcome to Classic! \n ")
+print("What is the meal of the day? ")
+print("You have 8 try's, have fun.\n ")
+
+main()
--- run.py
+++ run.py
@@ -1,3 +1,40 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+def user_input():
+    """
+    Ask user for a letter, prints out letter from user.
+    Repeats for 8 times. 
+    """
+    failed = ""
+    for i in range(3):
+        new_letter = input("Type a letter: ")
+        failed = failed + "[ " + new_letter + " ]"
+        correct = "_ _ _ a _"
+        padding = make_padding(failed, correct)
+        print(failed + padding + correct + "\n") 
+
+
+def make_padding(failed, correct):
+    """
+    Create a string that can be used as padding. The sum of the 
+    length of this string and the length of the two
+    parameter strings will be 30.
+    Example: make_padding("[d]", "_a_") returns 24 spaces 
+    """
+    padding = ""
+    length = 30 - len(failed) - len(correct)
+    for i in range(length):
+        padding = padding + " "
+    return padding
+
+
+def main():
+    """Run all program function"""
+    user_input()
+
+
+""" Program starts here """
+print("Welcome to Classic! \n ")
+print("What is the meal of the day? ")
+print("You have 8 try's, have fun.\n ")
+main()
